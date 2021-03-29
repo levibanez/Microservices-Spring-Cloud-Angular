9ba8422eaed0b525ddeb5f40f3c3659cf5b86ae2
Levi <levi_sl@hotmail.com>
2021-03-29 20:18:28 -0300

    microservices-users - Initial Commit - add student Entity

---
+++ microservices-users/src/main/java/br/com/levi/microservices/app/users/models/entity/Student.java
@@ -0,0 +1,72 @@
+package br.com.levi.microservices.app.users.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String nickname;
+	private String email;
+	
+	@Column(name = "create_at")
+	private Date createAt;
+
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+}
---
+++ microservices-users/src/test/java/br/com/levi/microservices/app/users/MicroservicesUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.levi.microservices.app.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
