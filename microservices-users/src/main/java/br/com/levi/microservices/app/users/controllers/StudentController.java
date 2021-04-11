4e1155a3a41ed33258ff1e4242b4a7823b2093fd
Levi <levi_sl@hotmail.com>
2021-04-11 11:44:20 -0300

    Implement CRUD in Student controller for post, put and delete

--- microservices-users/src/main/java/br/com/levi/microservices/app/users/controllers/StudentController.java
+++ microservices-users/src/main/java/br/com/levi/microservices/app/users/controllers/StudentController.java
@@ -3,9 +3,14 @@ package br.com.levi.microservices.app.users.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.levi.microservices.app.users.models.entity.Student;
@@ -17,10 +22,16 @@ public class StudentController {
 	@Autowired
 	private StudentService service;
 	
+	/**
+	 * List All Students
+	 * @return all students
+	 */
 	@GetMapping
 	public ResponseEntity<?> list() {
+		
 		return ResponseEntity.ok().body(service.findAll());
 	}
+	
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<?> get(@PathVariable Long id) {
@@ -28,9 +39,62 @@ public class StudentController {
 		Optional<Student> o = service.findById(id);
 		
 		if (o.isEmpty()) {
-			ResponseEntity.notFound().build();
+			return ResponseEntity.notFound().build();
 		}
 		
 		return ResponseEntity.ok(o.get());
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Student student) {
+		
+		Student studentDb = service.save(student);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(studentDb);
+	}
+	
+	@PutMapping("/{id}") 
+	public ResponseEntity<?> edit(@RequestBody Student student, @PathVariable Long id) {
+		
+		Optional<Student> o = service.findById(id);
+		
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Student studentDb = o.get();
+		studentDb.setName(student.getName());
+		studentDb.setNickname(student.getNickname());
+		studentDb.setEmail(student.getEmail());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(studentDb));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remove(@PathVariable Long id) {
+		
+		service.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
