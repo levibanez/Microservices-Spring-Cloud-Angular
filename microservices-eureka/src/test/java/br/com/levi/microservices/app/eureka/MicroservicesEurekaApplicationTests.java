a060dbe282ad10f4ab16bfa1c86fb92853851a24
Levi <levi_sl@hotmail.com>
2021-05-15 16:23:45 -0300

    Criando servidor de log Eureka

---
+++ microservices-eureka/src/test/java/br/com/levi/microservices/app/eureka/MicroservicesEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.levi.microservices.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
